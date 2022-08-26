f62f0c1ef0e722a13dd9fd531c688d585ba52c9f
zusibell <zusibell@gmail.com>
2022-08-27 00:47:50 +0200

    update

--- container/frontend/src/app/auth/oauth-config-factory.ts
+++ container/frontend/src/app/auth/oauth-config-factory.ts
@@ -2,7 +2,7 @@ import { OAuthModuleConfig } from "angular-oauth2-oidc";
 
 const createOAuthConfig: () => OAuthModuleConfig = (): OAuthModuleConfig => ({
         resourceServer: {
-          allowedUrls: ['http://localhost:3006/places'],
+          allowedUrls: ['http://localhost:3007'],
           sendAccessToken: true
         }
 })
