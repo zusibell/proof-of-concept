f5504195bae9c52df2fca1d1422d54f5160d897d
zusibell <zusibell@gmail.com>
2022-08-27 09:44:56 +0200

    change

--- remote/frontend/src/app/auth/auth.config.ts
+++ remote/frontend/src/app/auth/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   // Url of the Identity Provider
-  issuer: 'http://localhost:8080/realms/remote',
+  issuer: 'http://localhost:8080/realms/POC',
 
   // URL of the SPA to redirect the user to after login
   redirectUri: 'http://localhost:4100',
