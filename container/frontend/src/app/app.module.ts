955017271225457e0c58d44d6dc4b3643d735e83
zusibell <zusibell@gmail.com>
2022-08-27 11:47:00 +0200

    delete unused imports

--- container/frontend/src/app/app.module.ts
+++ container/frontend/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import {  CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AuthConfig, OAuthModule, OAuthModuleConfig } from 'angular-oauth2-oidc';
+import { AuthConfig, OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { authConfig } from './auth/auth.config';
 import { AuthModule } from './auth/auth.module';
-import createOAuthConfig from './auth/oauth-config-factory';
 import { AuthService } from './auth/auth.service';
-import { AppInterceptor } from './app.interceptor';
 
 
 
@@ -37,7 +35,6 @@ import { AppInterceptor } from './app.interceptor';
   ],
   providers: [
     { provide: AuthConfig, useValue: authConfig },
-    { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true }
   ],
   bootstrap: [AppComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
